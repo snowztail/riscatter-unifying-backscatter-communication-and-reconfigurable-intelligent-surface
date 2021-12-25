b4cdf7b0e08b51b5887faed8cd28c33bf24550b5
SnowzTail <i@snowztail.com>
2021-12-25 23:23:28 +0000

    :construction: Simulate basic DMTC

---
+++ src/lib/untitled.m
@@ -0,0 +1,35 @@
+nTxs = 3;
+nTags = 2;
+nStates = 4;
+[nInputs, nOutputs] = deal(nStates ^ nTags);
+reflectRatio = 0.5;
+nRatio = 10;
+constellation = phase_shift_keying(nStates);
+noiseVariance = 0;
+
+precoder = randn(nTxs, 1);
+precoder = precoder / norm(precoder);
+
+directChannel = sqrt(0.5) * (randn(1, nTxs) + 1i * randn(1, nTxs));
+cascadedChannel = zeros(nTags, nTxs);
+for iTag = 1 : nTags
+	cascadedChannel(iTag, :) = (sqrt(0.5) * (randn(1, nTxs) + 1i * randn(1, nTxs))) * (sqrt(0.5) * (randn + 1i * randn));
+end
+
+indexCombination = nested_combvec(1 : nStates, nTags);
+inputCombination = constellation(indexCombination);
+equivalentChannel = cell(nInputs, 1);
+for iInput = 1 : nInputs
+	equivalentChannel{iInput} = directChannel + sqrt(reflectRatio) * transpose(inputCombination(:, iInput)) * cascadedChannel;
+end
+[powerLevel, index] = sort(abs(cell2mat(equivalentChannel) * precoder) .^ 2);
+
+threshold = [0; nRatio * movmean(powerLevel, 2, 'Endpoints', 'discard'); Inf];
+
+dmtc = zeros(nInputs, nOutputs);
+for iInput = 1 : nInputs
+	conditionalEnergy = @(z) (z .^ (nRatio - 1) .* exp(-z ./ powerLevel(iInput))) ./ (powerLevel(iInput) .^ nRatio .* gamma(nRatio));
+	for iOutput = 1 : nOutputs
+		dmtc(iInput, iOutput) = integral(conditionalEnergy, threshold(iOutput), threshold(iOutput + 1));
+	end
+end

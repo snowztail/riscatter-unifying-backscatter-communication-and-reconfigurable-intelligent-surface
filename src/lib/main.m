57a180b7c8bddc6acf6686457de449fbc897f7e5
SnowzTail <i@snowztail.com>
2022-02-23 14:40:58 +0000

    :art: Refactor code

--- src/lib/main.m
+++ src/lib/main.m
@@ -10,7 +10,7 @@ noisePower = 1;
 nBins = 2 ^ 8;
 constellation = psk(nStates);
 confidenceScore = 10;
-tolerance = 1e-3;
+tolerance = eps;
 precoder = normc(randn(nTxs, 1));
 directChannel = sqrt(0.5) * (randn(1, nTxs) + 1i * randn(1, nTxs));
 cascadedChannel = zeros(nTags, nTxs);
@@ -41,85 +41,25 @@ end
 % * Discretize continuous output and remaps to DMC
 dmc = discretize_channel(thresholdCandidate, receivedPower, symbolRatio);
 
-% * Initialize input distribution
+% * Initialize input distribution, detection threshold, and DMTC
 inputDistribution = normr(sqrt(rand(nTags, nStates))) .^ 2;
 combinationDistribution = combination_distribution(inputDistribution);
 equivalentDistribution = prod(combinationDistribution, 1);
-[threshold, backscatterMutualInformation] = threshold_smawk(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio);
+[threshold, dmtc] = threshold_smawk(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio);
+[weightedSumRate, primaryRate, backscatterRate] = weighted_sum_rate(weight, symbolRatio, snr, equivalentDistribution, dmtc);
 
-% * Initialize detection thresholds, DMTC, and input distribution
-threshold = [0, thresholdCandidate(discretize(symbolRatio * movmean(receivedPower, 2, 'Endpoints', 'discard'), thresholdCandidate)), inf];
-% threshold2 = thresholdCandidate(discretize(symbolRatio * movmean(receivedPower, 2, 'Endpoints', 'discard'), thresholdCandidate));
-% threshold1 = [0, transpose(interp1(thresholdCandidate(1 : end - 1), thresholdCandidate(1 : end - 1), symbolRatio * movmean(receivedPower, 2, 'Endpoints', 'discard'), 'nearest')), inf];
-dmtc_ = discretize_channel(symbolRatio, receivedPower, threshold);
-dmtc_(mapIndex, mapIndex) = dmtc_;
-[wsr_, primaryRate, secondaryRate, inputDistribution, equivalentDistribution] = input_distribution(weight, symbolRatio, snr, dmtc_, nTags, nStates);
+% * Update input distribution and detection threshold alternatively
 isConverged = false;
 while ~isConverged
-	% * Eliminate unused letters
-	inputIndex = find(equivalentDistribution >= tolerance);
-	nOutputs = length(inputIndex);
-	p(mapIndex) = receivedPower;
-	[receivedPower1, mapIndex1] = sort(signalPower(inputIndex) + noisePower);
+	[inputDistribution, equivalentDistribution, weightedSumRate] = input_distribution(nTags, dmtc, weight, symbolRatio, snr);
+	[threshold, dmtc, backscatterRate] = threshold_smawk(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio);
+	[threshold1, dmtc1, backscatterRate1] = threshold_dp(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio);
+	[threshold2, dmtc2, backscatterRate2] = threshold_bisection(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio);
 
+	[weightedSumRate_, primaryRate, backscatterRate] = weighted_sum_rate(weight, symbolRatio, snr, equivalentDistribution, dmtc);
+	[weightedSumRate1_, primaryRate1, backscatterRate1] = weighted_sum_rate(weight, symbolRatio, snr, equivalentDistribution, dmtc1);
+	[weightedSumRate2_, primaryRate2, backscatterRate2] = weighted_sum_rate(weight, symbolRatio, snr, equivalentDistribution, dmtc2);
 
-	dmtc1 = dmtc_(inputIndex, inputIndex);
-	dmtc1 = dmtc1 ./ sum(dmtc1, 2);
-% 	[wsr0, primaryRate0, secondaryRate0, inputDistribution0, equivalentDistribution0] = input_distribution(weight, symbolRatio, snr(inputIndex), dmtc1, 1, nStates);
-	dmtc1(4, 4) = 0;
-	[wsr1, primaryRate1, secondaryRate1, inputDistribution1, equivalentDistribution] = input_distribution(weight, symbolRatio, snr, dmtc1, nTags, nStates);
-
-	% * Update thresholding scheme
-	[threshold4] = thresholding_smawk(dmc(inputIndex, :), equivalentDistribution(inputIndex), thresholdCandidate);
-	dmtc4 = discretize_channel(nInputs, nOutputs, symbolRatio, receivedPower, threshold4);
-	dmtc4(mapIndex, mapIndex1) = dmtc4;
-	dmtc4(dmtc4 < eps) = eps;
-	secondaryInformationFunction = secondary_information_function(equivalentDistribution, dmtc4);
-	mutualInformation4 = equivalentDistribution * secondaryInformationFunction;
-
-% 	[threshold_] = thresholding_smawk_1(dmc, equivalentDistribution, nOutputs, thresholdCandidate);
-	[threshold3] = thresholding_smawk(dmc, equivalentDistribution, thresholdCandidate);
-
-	[threshold_smawk, mutualInformation_smawk] = thresholding_smawk_1(dmc, equivalentDistribution, thresholdCandidate, symbolRatio, receivedPower, mapIndex);
-	[threshold_bis, mutualInformation_bis] = thresholding_bisection(dmc, equivalentDistribution, thresholdCandidate, symbolRatio, receivedPower, mapIndex);
-
-% 	[threshold_smawk4, mutualInformation_smawk4] = thresholding_smawk_1(dmc(inputIndex, :), equivalentDistribution(inputIndex), thresholdCandidate, symbolRatio, receivedPower, mapIndex);
-	% * Update DMTC
-% 	dmtc = discretize_channel(nOutputs, nOutputs, symbolRatio, receivedPower1, threshold);
-	dmtc = discretize_channel(nInputs, nOutputs, symbolRatio, receivedPower, threshold);
-% 	dmtc(mapIndex1, mapIndex1) = dmtc;
-	dmtc(mapIndex, mapIndex1) = dmtc;
-
-	dmtc3 = discretize_channel(nInputs, nInputs, symbolRatio, receivedPower, threshold3);
-	dmtc3(mapIndex, mapIndex) = dmtc3;
-
-% 	primaryInformationFunction = information_function_primary(symbolRatio, snr(inputIndex));
-% 	secondaryInformationFunction = secondary_information_function(equivalentDistribution(inputIndex), dmtc);
-% 	primaryRate = equivalentDistribution(inputIndex) * primaryInformationFunction;
-% 	secondaryRate = equivalentDistribution(inputIndex) * secondaryInformationFunction;
-% 	informationFunction = weight * primaryInformationFunction + (1 - weight) * secondaryInformationFunction;
-% 	mutualInformation = equivalentDistribution(inputIndex) * informationFunction;
-
-	% * Update tag input distribution
-	[wsr, primaryRate, secondaryRate, inputDistribution, equivalentDistribution] = input_distribution(weight, symbolRatio, snr, dmtc, nTags, nStates);
-	dmtc2 = dmtc;
-	dmtc2(4,4) = 0;
-	[wsr2, primaryRate2, secondaryRate2, inputDistribution2, equivalentDistribution2] = input_distribution(weight, symbolRatio, snr, dmtc2, nTags, nStates);
-
-	[wsr3, primaryRate3, secondaryRate3, inputDistribution3, equivalentDistribution3] = input_distribution(weight, symbolRatio, snr, dmtc3, nTags, nStates);
-
-	% * Test convergence
-	isConverged = abs(wsr - wsr_) <= tolerance;
-	wsr_ = wsr;
-end
-
-function [secondaryInformationFunction] = secondary_information_function(equivalentDistribution, dmtc)
-	[nInputs, nOutputs] = size(dmtc);
-	secondaryInformationFunction = zeros(nInputs, nOutputs);
-	for iInput = 1 : nInputs
-		for iOutput = 1 : nOutputs
-			secondaryInformationFunction(iInput, iOutput) = dmtc(iInput, iOutput) * log2(dmtc(iInput, iOutput) / (equivalentDistribution * dmtc(:, iOutput)));
-		end
-	end
-	secondaryInformationFunction = sum(secondaryInformationFunction, 2);
+	isConverged = abs(weightedSumRate_ - weightedSumRate) <= tolerance;
+	weightedSumRate = weightedSumRate_;
 end
---
+++ src/lib/weighted_sum_rate.m
@@ -0,0 +1,22 @@
+function [weightedSumRate, primaryRate, backscatterRate] = weighted_sum_rate(weight, symbolRatio, snr, equivalentDistribution, dmtc)
+	% Function:
+    %   - compute the weighted sum rate for a given input combination distribution and thresholding scheme
+    %
+    % Input:
+	%	- weight [2 * 1]: the relative priority of the primary and backscatter links
+	%	- symbolRatio: the ratio of the backscatter symbol period over the primary symbol period
+	%	- snr [(nStates ^ nTags) * 1]: signal-to-noise ratio of the primary link corresponding to to each input letter combination
+	%	- equivalentDistribution [1 * (nStates ^ nTags)]: equivalent input combination probability distribution
+    %   - dmtc [(nStates ^ nTags) * nOutputs]: the transition probability matrix of the backscatter discrete memoryless thresholding MAC
+    %
+    % Output:
+	%	- weightedSumRate: weighted sum of primary rate and total backscatter rate
+	%	- primaryRate: the achievable rate for the primary link (bps/Hz)
+	%	- backscatterRate: the achievable sum rate for the backscatter link (bpcu)
+    %
+    % Author & Date: Yang (i@snowztail.com), 22 Feb 22
+
+	primaryRate = equivalentDistribution * information_function_primary(symbolRatio, snr);
+	backscatterRate = equivalentDistribution * information_function_backscatter(equivalentDistribution, dmtc);
+	weightedSumRate = [primaryRate, backscatterRate] * weight;
+end

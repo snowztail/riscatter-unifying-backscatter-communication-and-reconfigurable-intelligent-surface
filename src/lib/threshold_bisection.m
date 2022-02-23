691a55068b55e6b267ceee41cbcc3cde1eb4bd3c
SnowzTail <i@snowztail.com>
2022-02-23 14:40:24 +0000

    :ambulance: Fix bisection thresholding algorithm

--- src/lib/threshold_bisection.m
+++ src/lib/threshold_bisection.m
@@ -1,4 +1,4 @@
-function [threshold, backscatterMutualInformation] = threshold_bisection(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio, tolerance)
+function [threshold, dmtc, backscatterRate] = threshold_bisection(thresholdCandidate, dmc, equivalentDistribution, receivedPower, symbolRatio, tolerance)
 	% Function:
 	%	- obtain the DMTC capacity-achieving thresholding scheme by bisection
     %
@@ -12,7 +12,8 @@ function [threshold, backscatterMutualInformation] = threshold_bisection(thresho
     %
     % Output:
 	%	- threshold [1 * nOutputs] : the optimal thresholding values
-	%	- backscatterMutualInformation: the optimal sum backscatter mutual information
+	%   - dmtc [(nStates ^ nTags) * nOutputs]: the transition probability matrix of the backscatter discrete memoryless thresholding MAC
+	%	- backscatterRate: the achievable sum rate for the backscatter link (bpcu)
     %
     % Comment:
     %   - For a given t_0 and t_1, the optimal t_j for j = 2, ..., K - 1 can be uniquely determined by bisection
@@ -35,35 +36,40 @@ function [threshold, backscatterMutualInformation] = threshold_bisection(thresho
 	nLevels = size(thresholdCandidate, 2);
 
 	% * Optimal t_0 = 0 and t_K+1 = ∞, traverse all possible t_1
-	backscatterMutualInformation = zero(nLevels, 1);
+	backscatterRateSet = zeros(nLevels, 1);
 	thresholdSet = cell(nLevels, 1);
-	for iLevel = 1 : nLevels
+	for iLevel = 2 : nLevels - 1
 		threshold = zeros(1, nOutputs + 1);
-		threshold(end) = Inf;
+		threshold(end) = inf;
 		threshold(2) = thresholdCandidate(iLevel);
 
 		% * Update t_j for j = 2, ..., K - 1 consequently
+		isValid = true;
 		for iThreshold = 3 : nOutputs
 			% * Compute reference divergence
-			binIndex = find(thresholdCandidate == threshold(iThreshold - 2)) : find(thresholdCandidate == threshold(iThreshold - 1));
-			binDensity = dmc(:, thresholdCandidate == threshold(iThreshold - 1));
-			divergence = backward_divergence(dmc, equivalentDistribution, binIndex, binDensity);
+			binIndex = find(thresholdCandidate == threshold(iThreshold - 2)) : find(thresholdCandidate == threshold(iThreshold - 1)) - 1;
+			thresholdDensity = threshold_density(receivedPower, symbolRatio, threshold(iThreshold - 1));
+			divergence = backward_divergence(dmc, equivalentDistribution, binIndex, thresholdDensity);
 
-			% * Bisection to guarantee (approximately) equal divergence
-			lowerThreshold = threshold(iThreshold - 1);
-			lowerBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == lowerThreshold);
-			lowerDivergence = backward_divergence(dmc, equivalentDistribution, lowerBinIndex, binDensity);
+			% * Initialize upper and lower bound of threshold
+			lowerThreshold = thresholdCandidate(find(thresholdCandidate == threshold(iThreshold - 1)) + 1);
+			upperThreshold = thresholdCandidate(end - 1);
+
+			lowerBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == lowerThreshold) - 1;
+			lowerDivergence = backward_divergence(dmc, equivalentDistribution, lowerBinIndex, thresholdDensity);
 			lowerDeviation = lowerDivergence - divergence;
 
-			upperThreshold = thresholdCandidate(end);
-			upperBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == upperThreshold);
-			upperDivergence = backward_divergence(dmc, equivalentDistribution, upperBinIndex, binDensity);
+			upperBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == upperThreshold) - 1;
+			upperDivergence = backward_divergence(dmc, equivalentDistribution, upperBinIndex, thresholdDensity);
 			upperDeviation = upperDivergence - divergence;
 
-			if (lowerDeviation < 0 && upperDeviation < 0) || (lowerDeviation > 0 && upperDeviation > 0)
-				error('Invalid initialization.');
+			if (lowerDeviation < 0 && upperDeviation < 0) || (lowerDeviation > 0 && upperDeviation > 0) || isnan(divergence)
+				isValid = false;
+				threshold(iThreshold : end - 1) = nan;
+				break;
 			end
 
+			% * Bisection to guarantee (approximately) equal divergence
 			while true
 				% * Tolerable deviation, exit loop and update threshold
 				if abs(lowerDeviation) <= tolerance || abs(upperDeviation) <= tolerance || find(thresholdCandidate == upperThreshold) - find(thresholdCandidate == lowerThreshold) <= 1
@@ -75,55 +81,55 @@ function [threshold, backscatterMutualInformation] = threshold_bisection(thresho
 					break;
 				end
 
-				% * Shrink search interval and continue looping
-				[~, midIndex] = min(abs(thresholdCandidate - mean([lowerThreshold, upperThreshold])));
-				midThreshold = threshold(midIndex);
-				midBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == midThreshold);
-				midDivergence = backward_divergence(dmc, equivalentDistribution, midBinIndex, binDensity);
+				% * Shrink search interval and update deviations
+				midThreshold = thresholdCandidate(round(mean([lowerBinIndex(end), upperBinIndex(end)])) + 1);
+				midBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == midThreshold) - 1;
+				midDivergence = backward_divergence(dmc, equivalentDistribution, midBinIndex, thresholdDensity);
 				midDeviation = midDivergence - divergence;
 				if midDeviation == 0
 					threshold(iThreshold) = midThreshold;
 					break;
 				elseif (lowerDeviation < 0 && midDeviation < 0 && upperDeviation > 0) || (lowerDeviation > 0 && midDeviation > 0 && upperDeviation < 0)
 					lowerThreshold = midThreshold;
+					lowerBinIndex = midBinIndex;
+					lowerDeviation = midDeviation;
 				elseif (lowerDeviation < 0 && midDeviation > 0 && upperDeviation > 0) || (lowerDeviation > 0 && midDeviation < 0 && upperDeviation < 0)
 					upperThreshold = midThreshold;
+					upperBinIndex = midBinIndex;
+					upperDeviation = midDeviation;
 				end
-
-				% * Update deviations
-				lowerBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == lowerThreshold);
-				lowerDivergence = backward_divergence(dmc, equivalentDistribution, lowerBinIndex, binDensity);
-				lowerDeviation = lowerDivergence - divergence;
-
-				upperBinIndex = find(thresholdCandidate == threshold(iThreshold - 1)) : find(thresholdCandidate == upperThreshold);
-				upperDivergence = backward_divergence(dmc, equivalentDistribution, upperBinIndex, binDensity);
-				upperDeviation = upperDivergence - divergence;
 			end
 		end
 
 		% * Construct DMTC and compute mutual information
-		dmtc = discretize_channel(threshold, receivedPower, symbolRatio);
-		backscatterMutualInformation(iLevel) = equivalentDistribution * information_function_backscatter(equivalentDistribution, dmtc);
-		thresholdSet{iLevel} = threshold;
+		if isValid
+			dmtc = discretize_channel(threshold, receivedPower, symbolRatio);
+			backscatterRateSet(iLevel) = equivalentDistribution * information_function_backscatter(equivalentDistribution, dmtc);
+			thresholdSet{iLevel} = threshold;
+		else
+			backscatterRateSet(iLevel) = nan;
+			thresholdSet{iLevel} = threshold;
+		end
 	end
-	[backscatterMutualInformation, optimalLevel] = max(backscatterMutualInformation);
+	[backscatterRate, optimalLevel] = max(backscatterRateSet);
 	threshold = thresholdSet{optimalLevel};
+	dmtc = discretize_channel(threshold, receivedPower, symbolRatio);
 end
 
 
-function [divergence] = backward_divergence(dmc, equivalentDistribution, binIndex, binDensity)
+function [divergence] = backward_divergence(dmc, equivalentDistribution, binIndex, thresholdDensity)
 	nInputs = size(equivalentDistribution, 2);
+	backwardThreshold = zeros(nInputs, 1);
 	backwardBin = zeros(nInputs, 1);
-	for iInput = 1 : nInputs
-		backwardBin(iInput) = equivalentDistribution(iInput) * binDensity(iInput) / (equivalentDistribution * binDensity);
-	end
-	backwardRegion = zeros(nInputs, 1);
-	for iInput = 1 : nInputs
-		backwardRegion(iInput) = equivalentDistribution(iInput) * sum(dmc(iInput, binIndex), 2) / (equivalentDistribution * sum(dmc(:, binIndex), 2));
-	end
 	divergence = zeros(nInputs, 1);
 	for iInput = 1 : nInputs
-		divergence(iInput) = backwardBin(iInput) * log(backwardBin(iInput) / backwardRegion(iInput));
+		backwardThreshold(iInput) = equivalentDistribution(iInput) * thresholdDensity(iInput) / (equivalentDistribution * thresholdDensity);
+		backwardBin(iInput) = equivalentDistribution(iInput) * sum(dmc(iInput, binIndex), 2) / (equivalentDistribution * sum(dmc(:, binIndex), 2));
+		divergence(iInput) = backwardThreshold(iInput) * log(backwardThreshold(iInput) / backwardBin(iInput));
 	end
 	divergence = sum(divergence, 1);
+end
+
+function [thresholdDensity] = threshold_density(receivedPower, symbolRatio, threshold)
+	thresholdDensity = (threshold .^ (symbolRatio - 1) .* exp(-threshold ./ receivedPower)) ./ (receivedPower .^ symbolRatio .* gamma(symbolRatio));
 end

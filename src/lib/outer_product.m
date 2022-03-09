d106e78e85817f576b9ea66547f6c335f656c0bb
SnowzTail <i@snowztail.com>
2022-03-09 18:48:36 +0000

    :zap: Update outer product function

--- src/lib/outer_product.m
+++ src/lib/outer_product.m
@@ -1,21 +1,22 @@
-function [outerProduct] = outer_product(varargin)
+function [outerProduct] = outer_product(matrix)
 	% Function:
-    %	- compute the outer product of arbitrary number of vectors
+    %	- compute the outer product of all column vectors of the input matrix
     %
     % Input:
-	%	- varargin: the cell containing input vectors
+	%	- matrix: columns as the input vectors
     %
     % Output:
 	%	- outerProduct: the outer product of input vectors
     %
     % Comment:
-    %	- Modified from https://stackoverflow.com/a/14356614/8891100
+    %	- modified from https://stackoverflow.com/a/14356614/8891100
     %
     % Author & Date: Yang (i@snowztail.com), 22 Mar 02
 
-    outerProduct = varargin{1};
-	for iVector = 2 : numel(varargin)
-		vector = permute(varargin{iVector}, circshift(1 : (ndims_modified(outerProduct) + 1), ndims_modified(outerProduct)));
+	nVectors = size(matrix, 2);
+    outerProduct = matrix(:, 1);
+	for iVector = 2 : nVectors
+		vector = permute(matrix(:, iVector), circshift(1 : (ndims_modified(outerProduct) + 1), ndims_modified(outerProduct)));
 		outerProduct = repmat(outerProduct, size(vector)) .* repmat(vector, size(outerProduct));
 	end
 end

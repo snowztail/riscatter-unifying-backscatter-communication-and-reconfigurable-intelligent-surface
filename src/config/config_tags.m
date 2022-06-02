afe844012f72afc9f6f76ea5615e935205ae6514
SnowzTail <i@snowztail.com>
2022-06-02 23:43:17 +0100

    :construction: Add scripts

---
+++ src/config/config_tags.m
@@ -0,0 +1,43 @@
+%% * System
+% number of transmit antennas
+nTxs = 4;
+% % number of tags
+% nTags = 5;
+% number of available states at tags (i.e., modulation order)
+nStates = 2;
+% constellation diagram at tags
+constellation = normalize(qammod(transpose(0 : nStates - 1), nStates), 'norm', Inf);
+% carrier frequency
+frequency = 9e8;
+% amplitude scatter ratio at tags
+scatterRatio = 0.5;
+% backscatter/primary symbol duration ratio
+symbolRatio = 10;
+% average transmit power
+transmitPower = db2pow(6);
+% average noise power
+noisePower = db2pow(-100);
+% antenna gain
+rxGain = db2pow(3);
+% % layout and distance
+% [directDistance, forwardDistance, backwardDistance] = layout(nTags);
+% path loss exponents
+directExponent = 2.6;
+forwardExponent = 2.4;
+backwardExponent = 2;
+% Ricean factors
+directFactor = 5;
+forwardFactor = 5;
+backwardFactor = 10;
+
+
+%% * Algorithm
+% relative priority of primary link
+weightSet = [0 : 1e-3 : 2e-2, 1];
+% number of weights on rate region boundary
+nWeights = length(weightSet);
+
+
+%% * Variable
+Variable.nTags = 2 .^ (0 : 3);
+nVariables = length(Variable.nTags);
---
+++ src/pbs/region_tags.sh
@@ -0,0 +1,10 @@
+#PBS -l walltime=72:00:00,select=1:ncpus=1:ompthreads=1:mem=4gb
+#PBS -N region_tags
+#PBS -J 1-300
+
+module load matlab/R2021b
+
+cd $PBS_O_WORKDIR/..
+matlab -nodesktop -nodisplay -nosplash -singleCompThread < region_tags.m
+
+exit

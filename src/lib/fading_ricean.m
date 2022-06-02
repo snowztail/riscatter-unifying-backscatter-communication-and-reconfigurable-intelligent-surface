035dba0312a26e7b9e7df8ba60faa895c73b47ae
SnowzTail <i@snowztail.com>
2022-06-02 20:44:06 +0100

    :ambulance: Fix Ricean fading

--- src/lib/fading_ricean.m
+++ src/lib/fading_ricean.m
@@ -1,4 +1,4 @@
-function [fading] = fading_ricean(nTxs, nRxs, factor, aoa)
+function [fading] = fading_ricean(nTxs, nRxs, factor, phase)
 	% Function:
 	%	- simulate small-scale fading in i.i.d. Ricean distribution
     %
@@ -6,7 +6,7 @@ function [fading] = fading_ricean(nTxs, nRxs, factor, aoa)
 	%	- nTxs: number of transmit antennas
 	%	- nRxs: number of receive antennas
 	%	- factor: Ricean factor
-	%	- aoa: angle of arrival
+	%	- phase: phase shift of direct path between transmitter and receiver
     %
     % Output:
 	%	- fading [nTxs x nRxs]: small-scale signal attenuation
@@ -21,9 +21,9 @@ function [fading] = fading_ricean(nTxs, nRxs, factor, aoa)
 		nTxs;
 		nRxs;
 		factor;
-		aoa = rand * 2 * pi;
+		phase = rand * 2 * pi;
 	end
 
 	% * Simulate Ricean fading
-	fading = sqrt(0.5 * factor / (factor + 1)) * exp(1i * aoa) + sqrt(0.5 / (factor + 1)) * (randn(nTxs, nRxs) + 1i * randn(nTxs, nRxs));
+	fading = sqrt(factor / (factor + 1)) * exp(1i * phase) + sqrt(0.5 / (factor + 1)) * (randn(nTxs, nRxs) + 1i * randn(nTxs, nRxs));
 end

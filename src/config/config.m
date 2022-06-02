7eff825719e3ff76b50eba35d926bfe60cd6b99b
SnowzTail <i@snowztail.com>
2022-06-02 20:41:58 +0100

    :wrench: Update setup

--- src/config/config.m
+++ src/config/config.m
@@ -1,56 +1,38 @@
 %% * System
-% carrier frequency
-frequency = 9e8;
 % number of transmit antennas
-nTxs = 3;
+nTxs = 4;
 % number of tags
-nTags = 2;
+nTags = 5;
 % number of available states at tags (i.e., modulation order)
 nStates = 2;
-% number of possible input and output tuples (i.e., alphabet size)
-[nInputs, nOutputs] = deal(nStates ^ nTags);
+% constellation diagram at tags
+constellation = normalize(qammod(transpose(0 : nStates - 1), nStates), 'norm', Inf);
+% carrier frequency
+frequency = 9e8;
 % amplitude scatter ratio at tags
 scatterRatio = 0.5;
 % backscatter/primary symbol duration ratio
 symbolRatio = 10;
-% constellation diagram at tags
-constellation = normalize(qammod(transpose(0 : nStates - 1), nStates), 'norm', Inf);
-% constellation = transpose(exp(1i * 2 * pi * (0 : nStates - 1) / nStates));
 % average transmit power
 transmitPower = db2pow(6);
-% transmitPower = 1e3;
 % average noise power
 noisePower = db2pow(-100);
+% antenna gain
+rxGain = db2pow(3);
+% layout and distance
+[directDistance, forwardDistance, backwardDistance] = layout(nTags);
 % path loss exponents
 directExponent = 2.6;
 forwardExponent = 2.4;
 backwardExponent = 2;
-% angle of arrivals
-directAoa = 2 * pi * rand;
-forwardAoa = 2 * pi * rand(nTags, 1);
-backwardAoa = 2 * pi * rand(nTags, 1);
 % Ricean factors
 directFactor = 5;
 forwardFactor = 5;
-backwardFactor = 5;
+backwardFactor = 10;
 
-%% * Layout
-% coordinates
-% ap = [5; 0];
-% user = [0; 0];
-% [tag(1, :), tag(2, :)] = pol2cart(2 * pi * rand(1, nTags), sqrt(1 * rand(1, nTags)));
-ap = [2; 0];
-user = [0; 0];
-tag(1, :) = ones(1, nTags);
-tag(2, :) = (1 : nTags) - mean(1 : nTags);
-% distances
-directDistance = norm(ap - user);
-forwardDistance = vecnorm(ap - tag);
-backwardDistance = vecnorm(tag - user);
 
 %% * Algorithm
 % relative priority of primary link
-% weightSet = 0.1 : 0.05 : 1;
-weightSet = 0 : 1e-4 : 1;
+weightSet = [0 : 1e-3 : 2e-2, 1];
 % number of weights on rate region boundary
 nWeights = length(weightSet);
--- src/setup.m
+++ src/setup.m
@@ -1,8 +1,13 @@
 addpath(genpath(pwd));
-% rmpath('toolbox/CVX/');
+
 set(groot, 'defaulttextinterpreter', 'latex');
 set(groot, 'defaultaxesticklabelinterpreter', 'latex');
 set(groot, 'defaultaxesfontname', 'latex');
 set(groot, 'defaultlegendinterpreter', 'latex');
 set(groot, 'defaultlinelinewidth', 2);
 set(groot, 'defaultstemlinewidth', 2);
+
+% PBS_ARRAY_INDEX is environment variable: 0 means local and positive integer means HPC instance index
+iInstance = str2double(getenv('PBS_ARRAY_INDEX'));
+disp(iInstance);
+rng(iInstance);

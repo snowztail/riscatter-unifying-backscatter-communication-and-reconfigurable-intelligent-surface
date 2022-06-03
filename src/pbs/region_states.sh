aa64057136cd992a7a1eb4b42ff2e9c8727b27b6
SnowzTail <i@snowztail.com>
2022-06-03 17:21:55 +0100

    :balloon: Update pbs scripts

--- src/pbs/region_states.sh
+++ src/pbs/region_states.sh
@@ -1,8 +1,6 @@
-#PBS -l walltime=72:00:00,select=1:ncpus=1:ompthreads=1:mem=4gb
+#PBS -l walltime=24:00:00,select=1:ncpus=1:ompthreads=1:mem=4gb
 #PBS -N region_states
 #PBS -J 1-300
-#PBS -e ${PBS_O_WORKDIR}/log/err_${PBS_JOBID}.txt
-#PBS -o ${PBS_O_WORKDIR}/log/out_${PBS_JOBID}.txt
 
 module load matlab/R2021a
 
--- src/pbs/region_txs.sh
+++ src/pbs/region_txs.sh
@@ -1,10 +1,10 @@
 #PBS -l walltime=24:00:00,select=1:ncpus=1:ompthreads=1:mem=4gb
-#PBS -N region_tags
+#PBS -N region_txs
 #PBS -J 1-300
 
 module load matlab/R2021a
 
 cd $PBS_O_WORKDIR/..
-matlab -nodesktop -nodisplay -nosplash -singleCompThread < region_tags.m
+matlab -nodesktop -nodisplay -nosplash -singleCompThread < region_txs.m
 
 exit

6eb0207d964b81eede6df03d1d7572100818b117
SnowzTail <i@snowztail.com>
2022-06-06 22:43:16 +0100

    :balloon: Update pbs scripts

--- src/pbs/region_txs.sh
+++ src/pbs/region_txs.sh
@@ -1,10 +1,10 @@
 #PBS -l walltime=24:00:00,select=1:ncpus=1:ompthreads=1:mem=4gb
-#PBS -N region_txs
 #PBS -J 1-300
 
 module load matlab/R2021a
 
 cd $PBS_O_WORKDIR/..
-matlab -nodesktop -nodisplay -nosplash -singleCompThread < region_txs.m
+# matlab -batch $(basename $BASH_SOURCE .sh)
+matlab -batch "region_txs"
 
 exit

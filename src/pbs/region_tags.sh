04c1bb8a88d773319dda97104451ff091c56a448
SnowzTail <i@snowztail.com>
2022-06-07 18:00:16 +0100

    :balloon: Update pbs scripts

--- src/pbs/region_tags.sh
+++ src/pbs/region_tags.sh
@@ -5,6 +5,6 @@ module load matlab/R2021a
 
 cd $PBS_O_WORKDIR/..
 # matlab -batch $(basename $BASH_SOURCE .sh)
-matlab -batch "region_tags"
+matlab -batch -singleCompThread "region_tags"
 
 exit
--- src/pbs/region_txs.sh
+++ src/pbs/region_txs.sh
@@ -5,6 +5,6 @@ module load matlab/R2021a
 
 cd $PBS_O_WORKDIR/..
 # matlab -batch $(basename $BASH_SOURCE .sh)
-matlab -batch "region_txs"
+matlab -batch -singleCompThread "region_txs"
 
 exit

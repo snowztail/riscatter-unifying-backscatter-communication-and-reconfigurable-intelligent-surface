04a53fd47fa73055746ec79181d226d3eecd4234
SnowzTail <i@snowztail.com>
2022-06-08 20:40:55 +0100

    :balloon: Update pbs scripts

---
+++ src/pbs/region_duration.sh
@@ -0,0 +1,10 @@
+#PBS -l walltime=24:00:00,select=1:ncpus=1:ompthreads=1:mem=4gb
+#PBS -J 1-300
+
+module load matlab/R2021a
+
+cd $PBS_O_WORKDIR/..
+# matlab -batch $(basename $BASH_SOURCE .sh)
+matlab -singleCompThread -batch "region_duration"
+
+exit

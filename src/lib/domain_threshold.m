b662f05ed0e10498b089219a9f66ba34d5075694
SnowzTail <i@snowztail.com>
2022-06-14 21:57:45 +0100

    :art: Update threshold designs

--- src/lib/domain_threshold.m
+++ src/lib/domain_threshold.m
@@ -1,33 +1,42 @@
-function [thresholdDomain] = domain_threshold(symbolRatio, receivePower, confidence, nBins)
+function [thresholdDomain] = domain_threshold(symbolRatio, receivePower, nBins, confidence, tolerance)
 	% Function:
-    %	- determine (high-resolution) quantization levels to quantize receive energy into numerous bins
-	%	- quantization levels act as domain of decision thresholds
+    %	- design (high-resolution) quantization set that act as domain of decision thresholds
     %
     % Input:
 	%	- symbolRatio: the ratio of the backscatter symbol period over the primary symbol period
 	%	- receivePower [nInputs x 1]: average receive power per primary symbol for each tag state tuple
-	%	- confidence: coefficient to balance precision and underflow
 	%	- nBins: number of receive energy quantization bins
+	%	- confidence: confidence level for edge hypotheses
+	%	- tolerance: tail probability (i.e., 1 - confidence level) of replacing infinity by critical threshold
     %
     % Output:
 	%	- thresholdDomain [1 x (nBins + 1)]: boundaries of quantized energy bins as domain of decision thresholds
     %
     % Comment:
-	%	- quantized receive energy bins should be grouped to formulate decision regions
-	%	- hence, decision thresholds are selected from quantization levels
-	%	- high-resolution quantization levels improves detection performance at the cost of (linear) computation complexity
+	%	- discretize receive energy into numerous bins for threshold design
+	%	- decision region of each letter consists of adjoint energy bins, and thresholds are selected from quantization set
+	%	- high-resolution quantization improves detection performance at the cost of (linear) computation complexity
+	%	- select quantizer uniformly over critical region associated with confidence level of edge hypotheses to improve performance
+	%	- replace infinity by critical value of confidence approaching 1 (as infinity is not supported in PGD beamforming design)
     %
     % Author & Date: Yang (i@snowztail.com), 22 Apr 18
 
-	% * Set default bin number and confidence score
+	% * Set default bin number, confidence level and tolerance
 	arguments
 		symbolRatio;
 		receivePower;
-		confidence = 10;
 		nBins = 2 ^ 8;
+		confidence = 0.95;
+		tolerance = eps;
 	end
 
-	% * Use ML threshold as reference
-	threshold = threshold_ml(symbolRatio, receivePower);
-	thresholdDomain = linspace(threshold(2) / confidence, threshold(end - 1) * confidence, nBins + 1);
+	% * Compute lower and upper critical thresholds
+	tlb = icdf('Gamma', 1 - confidence, symbolRatio, min(receivePower));
+	tub = icdf('Gamma', confidence, symbolRatio, max(receivePower));
+
+	% * Replace infinity by critical threshold of confidence approaching 1
+	tib = icdf('Gamma', 1 - tolerance, symbolRatio, max(receivePower));
+
+	% * Design threshold domain
+	thresholdDomain = [0, linspace(tlb, tub, nBins - 1), tib];
 end
--- src/lib/threshold_ml.m
+++ src/lib/threshold_ml.m
@@ -1,39 +1,38 @@
-function [threshold] = threshold_ml(symbolRatio, receivePower, confidence)
+function [threshold] = threshold_ml(symbolRatio, receivePower, tolerance)
 	% Function:
 	%	- obtain the decision thresholds for maximum likelihood detector
     %
     % Input:
 	%	- symbolRatio: backscatter/primary symbol duration ratio
 	%	- receivePower [nInputs x 1]: average receive power per primary symbol for each tag state tuple
-	%	- confidence: coefficient to balance precision and underflow
+	%	- tolerance: tail probability (i.e., 1 - confidence level) of replacing infinity by critical threshold
     %
     % Output:
 	%	- threshold [1 x (nOutputs + 1)]: boundaries of ML decision regions (including 0 and Inf)
     %
     % Comment:
-    %	- ML detector does not depend on input distribution
-	%	- replace infinity by finite threshold to avoid precision issue (underflow)
+	%	- for completness, zero and infinity are defined as first and last thresholds
+    %	- other thresholds depend on average receiver power of (power-sorted) adjacent letters, and
+	%	- threshold design is not influenced by input distribution
+	%	- replace infinity by critical threshold of confidence approaching 1 (as infinity is not supported in PGD beamforming design)
     %
     % Author & Date: Yang (i@snowztail.com), 23 Mar 17
 
-	% * Set default confidence score
+	% * Set default tolerance
 	arguments
 		symbolRatio;
 		receivePower;
-		confidence = 10;
+		tolerance = eps;
 	end
 
 	% * Get data
 	nOutputs = size(receivePower, 1);
 
-	% * Each ML threshold depends on average receive power of adjacent letters
+	% * Design ML thresholds
 	sortPower = sort(receivePower);
-
-	% ! Replace infinity by finite threshold to avoid precision issue (underflow)
-	threshold = zeros(1, nOutputs + 1);
+	% threshold(1) = 0;
+	threshold(nOutputs + 1) = icdf('Gamma', 1 - tolerance, symbolRatio, max(receivePower));
 	for iOutput = 2 : nOutputs
 		threshold(iOutput) = symbolRatio * (sortPower(iOutput - 1) * sortPower(iOutput)) / (sortPower(iOutput - 1) - sortPower(iOutput)) * log(sortPower(iOutput - 1) / sortPower(iOutput));
 	end
-	% threshold(nOutputs + 1) = Inf;
-	threshold(end) = confidence * threshold(end - 1);
 end

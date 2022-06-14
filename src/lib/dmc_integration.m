1f675307fba438f3c3773a2dd2f68760af03e511
SnowzTail <i@snowztail.com>
2022-06-14 21:55:10 +0100

    :zap: Optimize DMC integration

--- src/lib/dmc_integration.m
+++ src/lib/dmc_integration.m
@@ -16,14 +16,15 @@ function [dmc] = dmc_integration(symbolRatio, receivePower, threshold)
     %
     % Author & Date: Yang (i@snowztail.com), 22 Feb 09
 
-	% * Get data
-	nInputs = size(receivePower, 1);
-	nOutputs = size(threshold, 2) - 1;
+	% % * Get data
+	% nInputs = size(receivePower, 1);
+	% nOutputs = size(threshold, 2) - 1;
 
-	% * Construct DMC based on conditional energy p.d.f. and decision thresholds
-	dmc = zeros(nInputs, nOutputs);
-	channelDistribution = @(z) (z .^ (symbolRatio - 1) .* exp(-z ./ receivePower)) ./ (receivePower .^ symbolRatio .* gamma(symbolRatio));
-	for iOutput = 1 : nOutputs
-		dmc(:, iOutput) = integral(channelDistribution, threshold(iOutput), threshold(iOutput + 1), 'ArrayValued', true);
-	end
+	% % * Construct DMC based on conditional energy p.d.f. and decision thresholds
+	% dmc = zeros(nInputs, nOutputs);
+	% channelDistribution = @(z) (z .^ (symbolRatio - 1) .* exp(-z ./ receivePower)) ./ (receivePower .^ symbolRatio .* gamma(symbolRatio));
+	% for iOutput = 1 : nOutputs
+	% 	dmc(:, iOutput) = integral(channelDistribution, threshold(iOutput), threshold(iOutput + 1), 'ArrayValued', true);
+	% end
+	dmc = diff(cdf('Gamma', threshold, symbolRatio, receivePower), [], 2);
 end

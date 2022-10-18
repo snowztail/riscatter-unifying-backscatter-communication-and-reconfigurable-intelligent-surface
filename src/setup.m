f908eadbc2e07a6e750ec19acbed0314e301a0e7
SnowzTail <i@snowztail.com>
2022-10-18 13:13:52 +0100

    :paintbrush: Update plot functions

--- src/setup.m
+++ src/setup.m
@@ -6,6 +6,7 @@ set(groot, 'defaultaxesfontname', 'latex');
 set(groot, 'defaultlegendinterpreter', 'latex');
 set(groot, 'defaultlinelinewidth', 2);
 set(groot, 'defaultstemlinewidth', 2);
+set(groot, 'defaultscatterlinewidth', 2);
 
 % PBS_ARRAY_INDEX is environment variable: 0 means local and positive integer means HPC instance index
 iInstance = str2double(getenv('PBS_ARRAY_INDEX'));
